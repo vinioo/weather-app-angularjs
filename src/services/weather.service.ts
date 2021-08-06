76c6eac18628944b9aa60b3fb501e815d5de1fc2
Vinicius Oliveira Souza <viniciusos@take.net>
2021-08-06 13:34:32 -0300

    add filter

--- src/services/weather.service.ts
+++ src/services/weather.service.ts
@@ -2,17 +2,16 @@ import { IHttpService } from "angular";
 
 const apiUrl = "https://api.weatherbit.io/v2.0/forecast/daily";
 export const iconApiUrl = "https://www.weatherbit.io/static/img/icons/";
+const apiKey = "5490d8df7dba44ca8af337baa984ba31";
 
 export class WeatherService {
-    static selector = "weatherService";
-
     constructor(private $http: IHttpService) {}
 
     public async getForecast(city: string) {
         return this.$http({
             method: "get",
             url: apiUrl,
-            params: { key: "5490d8df7dba44ca8af337baa984ba31", days: "5", city },
+            params: { key: apiKey, days: "5", city },
         });
     }
 }
